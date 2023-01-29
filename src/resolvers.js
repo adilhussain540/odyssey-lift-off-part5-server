a15b69b03a437c54e3e28a4e796d8d5ee193702d
adilhussain540 <adilhussain540@gmail.com>
2023-01-29 13:04:30 +0500

    Add a new field durationInSeconds in Track and Module types

--- src/resolvers.js
+++ src/resolvers.js
@@ -36,14 +36,19 @@ const resolvers = {
       }
     },
   },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: ({ length }) => length,
+  },
+
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
